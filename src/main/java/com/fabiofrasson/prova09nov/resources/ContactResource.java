3f9da1dae53500bb18140e6e50c7c0717c741396
Fabio Frasson <fabio.frass@gmail.com>
2021-11-09 20:14:08 -0300

    Project scaffolding

---
+++ src/main/java/com/fabiofrasson/prova09nov/resources/ContactResource.java
@@ -0,0 +1,60 @@
+package com.fabiofrasson.prova09nov.resources;
+
+import com.fabiofrasson.prova09nov.models.Contact;
+import com.fabiofrasson.prova09nov.services.ContactService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/api/contacts")
+public class ContactResource {
+
+  private final ContactService service;
+
+  public ContactResource(ContactService service) {
+    this.service = service;
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Contact>> list() {
+    return ResponseEntity.ok(service.listAll());
+  }
+
+  @GetMapping(path = "/id/{id}")
+  public ResponseEntity<Contact> findById(@PathVariable("id") Long id) {
+    return ResponseEntity.ok(service.findByIdOrThrowResourceNotFoundException(id));
+  }
+
+  @GetMapping(path = "/email/{email}")
+  public ResponseEntity<Optional<Contact>> findByEmail(@PathVariable("email") String email) {
+    return ResponseEntity.ok(service.findByEmail(email));
+  }
+
+  @GetMapping(path = "/phone/{phone}")
+  public ResponseEntity<Optional<Contact>> findByPhone(@PathVariable("phone") String phone) {
+    return ResponseEntity.ok(service.findByPhoneNumber(phone));
+  }
+
+  @PostMapping
+  public ResponseEntity<Contact> save(@RequestBody @Valid Contact contact) {
+    return new ResponseEntity<>(service.save(contact), HttpStatus.CREATED);
+  }
+
+  @DeleteMapping(path = "/{id}")
+  public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
+    service.delete(id);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+
+  @PutMapping
+  public ResponseEntity<Void> replace(@RequestBody @Valid Contact contact) {
+    service.replace(contact);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+}
--- src/main/java/com/fabiofrasson/prova09nov/services/ContactService.java
+++ src/main/java/com/fabiofrasson/prova09nov/services/ContactService.java
@@ -1,11 +1,14 @@
 package com.fabiofrasson.prova09nov.services;
 
+import com.fabiofrasson.prova09nov.exceptions.ResourceAlreadyExistsException;
 import com.fabiofrasson.prova09nov.exceptions.ResourceNotFoundException;
 import com.fabiofrasson.prova09nov.models.Contact;
 import com.fabiofrasson.prova09nov.repositories.ContactRepository;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ContactService {
@@ -24,5 +27,41 @@ public class ContactService {
     return repository
         .findById(id)
         .orElseThrow(() -> new ResourceNotFoundException("Resource already exists."));
+  }
+
+  public Optional<Contact> findByEmail(String email) {
+    return repository.findContactByEmailIgnoreCase(email);
+  }
+
+  public Optional<Contact> findByPhoneNumber(String phoneNumber) {
+    return repository.findContactByPhoneNumber(phoneNumber);
+  }
+
+  @Transactional
+  public Contact save(Contact contact) {
+    Optional<Contact> contactByEmail = repository.findContactByEmailIgnoreCase(contact.getEmail());
+    Optional<Contact> contactByPhoneNumber =
+        repository.findContactByPhoneNumber(contact.getPhoneNumber());
+
+    if (contactByEmail.isPresent() || contactByPhoneNumber.isPresent()) {
+      throw new ResourceAlreadyExistsException(
+          "Contact already exists. Please try again with different email and/or phone number.");
+    } else {
+      return repository.save(contact);
+    }
+  }
+
+  public void delete(Long id) {
+    repository.delete(findByIdOrThrowResourceNotFoundException(id));
+  }
+
+  public void replace(Contact contact) {
+    Optional<Contact> searchedContact = repository.findContactByEmailIgnoreCase(contact.getEmail());
+
+    if (searchedContact.isEmpty()) {
+      throw new ResourceNotFoundException("Contact not found. Please try again.");
+    } else {
+      repository.save(contact);
+    }
   }
 }
